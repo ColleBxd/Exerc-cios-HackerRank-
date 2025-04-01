a539c9c1c3d7d8b82b450b079c65cbac78d15db8
Colle <colleluizz@gmail.com>
2025-04-01 18:45:56 -0300

    Add files via upload

---
+++ EX1/src/main/java/com/aula/Aula.java
@@ -0,0 +1,25 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.aula;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author colle
+ */
+public class Aula {
+
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+        int a = scan.nextInt();
+        int b = scan.nextInt();
+        int c = scan.nextInt();
+
+        System.out.println(a);
+        System.out.println(b);
+        System.out.println(c);
+    }
+}
